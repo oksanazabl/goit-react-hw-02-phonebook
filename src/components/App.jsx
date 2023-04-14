e7a2c2d172a83301b3c2e30c0360510dedb2e00c
OksanaZabl <73551722+oksanazabl@users.noreply.github.com>
2023-04-14 23:28:58 +0300

    add npm

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import {Component} from 'react';
+import Section from './Section';
+// import Counter from './FeedbackOptions';
+// import Statistics from './Statistics';
+// import Notification from './Notification';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    name: ''
+  }
+
+ 
+  render() {
+    return (
+      <div>
+        <Section title="Phonebook">
+         
+        </Section>
+        <Section title="Cnotacts">
+         
+        </Section>
+      </div>
+    );
+  }
+}
+
+export default App;
