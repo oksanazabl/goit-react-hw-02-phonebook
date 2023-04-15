0b8e42f374ad122a827797d1352712db7d418ab0
OksanaZabl <73551722+oksanazabl@users.noreply.github.com>
2023-04-15 23:26:24 +0300

    step 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,48 @@
-import {Component} from 'react';
-import Section from './Section';
-// import Counter from './FeedbackOptions';
-// import Statistics from './Statistics';
-// import Notification from './Notification';
+import { nanoid } from 'nanoid';
+import { Component } from 'react';
 
 class App extends Component {
   state = {
     contacts: [],
     name: ''
-  }
+  };
 
- 
+  handleNameChange = event => {
+    this.setState({ name: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const { name, contacts } = this.state;
+    const newContact = { id: nanoid(), name };
+    this.setState({ contacts: [...contacts, newContact], name: '' });
+  };
+
   render() {
+    const { name, contacts } = this.state;
+
     return (
       <div>
-        <Section title="Phonebook">
-         
-        </Section>
-        <Section title="Cnotacts">
-         
-        </Section>
+        <h1>Phonebook</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="nameInput">Name</label>
+          <input
+            type="text"
+            id="nameInput"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            value={name}
+            onChange={this.handleNameChange}
+          />
+          <button type="submit">Add Contact</button>
+        </form>
+        <ul>
+          {contacts.map(contact => (
+            <li key={contact.id}>{contact.name}</li>
+          ))}
+        </ul>
       </div>
     );
   }
