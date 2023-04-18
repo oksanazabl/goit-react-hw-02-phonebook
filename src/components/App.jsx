9e9d92cb7e3e0ea12f971b0319299f866a27b274
OksanaZabl <73551722+oksanazabl@users.noreply.github.com>
2023-04-18 22:40:51 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,48 +1,21 @@
 import { Component } from 'react';
-import { nanoid } from 'nanoid';
+// import { nanoid } from 'nanoid';
 import Container from './Container';
 import PhonebookForm from './PhonebookForm';
 import PhonebookContacts from './PhonebookContatcs';
 import PhonebookFilter from './PhonebookFilter';
 import filterContacts from '../utils/filterContacts';
-import isContactExist from '../utils/isContactExist';
+
 
 class App extends Component {
   state = {
     contacts: [],
     filter: '',
-    name: '',
-    number: '',
+    // name: '',
+    // number: '',
   };
 
-  handleChange = event => {
-    const { name, value } = event.target;
-    this.setState({ [name]: value });
-  };
-
-  handleSubmit = event => {
-    event.preventDefault();
-
-    const { name, number,contacts } = this.state;
-    const id = nanoid();
-    // const contact = { id, name, number };
-
-    if (isContactExist(name, contacts)) {
-      alert(`${name} is already in contacts`);
-      return;
-    }
-    const newContact = {
-      id: id,
-      name,
-      number,
-    };
-
-    this.setState(prevState => ({
-      contacts: [newContact,...prevState.contacts],
-      name: '',
-      number: '',
-    }));
-  };
+  
 
   handleFilterChange = event => {
     this.setState({ filter: event.target.value });
@@ -55,16 +28,16 @@ class App extends Component {
   };
 
   render() {
-    const { name, contacts, number, filter } = this.state;
+    const { contacts,filter } = this.state;
     const newContacts = filterContacts(contacts, filter);
     return (
       <Container>
         <h1>Phonebook</h1>
         <PhonebookForm
-          name={name}
-          number={number}
-          handleSubmit={this.handleSubmit}
-          handleChange={this.handleChange}
+          name={this.state.name}
+          number={this.state.number}
+          // handleSubmit={this.handleSubmit}
+          // handleChange={this.handleChange}
         />
         <h2>Contacts</h2>
         <PhonebookFilter
--- src/components/PhonebookForm/PhonebookForm.jsx
+++ src/components/PhonebookForm/PhonebookForm.jsx
@@ -1,47 +1,85 @@
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import css from './PhonebookForm.module.css';
+import { nanoid } from 'nanoid';
+import { Component } from 'react';
+import isContactExist from '../../utils/isContactExist';
 
-const PhonebookForm = ({ handleSubmit, name, handleChange, number }) => {
-  return (
-    <form className={css.form} onSubmit={handleSubmit}>
-      <label className={css.label} htmlFor="name">
-        Name:
-      </label>
-      <input
-        className={css.input}
-        type="text"
-        name="name"
-        value={name}
-        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-        required
-        onChange={handleChange}
-        placeholder="Name"
-      />
-      <label className={css.label} htmlFor="number">
-        Phone number:
-      </label>
-      <input
-        className={css.input}
-        type="tel"
-        name="number"
-        value={number}
-        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-        required
-        onChange={handleChange}
-        placeholder="Phone number"
-      />
-      <button className={css.button} type="submit">Add Contact</button>
-    </form>
-  );
-};
+class PhonebookForm extends Component {
+  state = {
+    contacts: [],
+    name: '',
+    number: '',
+  };
 
-PhonebookForm.propTypes = {
-  handleSubmit: PropTypes.func.isRequired,
-  name: PropTypes.string.isRequired,
-  handleChange: PropTypes.func.isRequired,
-  number: PropTypes.string.isRequired,
-};
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    const { name, number, contacts } = this.state;
+    const id = nanoid();
+    // const contact = { id, name, number };
+
+    if (isContactExist(name, contacts)) {
+      alert(`${name} is already in contacts`);
+      return;
+    }
+    const newContact = {
+      id: id,
+      name,
+      number,
+    };
+
+    this.setState(prevState => ({
+      contacts: [newContact, ...prevState.contacts],
+      name: '',
+      number: '',
+    }));
+  };
+
+  render() {
+    const { name, number } = this.state;
+    return (
+      <form className={css.form} onSubmit={this.handleSubmit}>
+        <label className={css.label} htmlFor="name">
+          Name:
+        </label>
+        <input
+          className={css.input}
+          type="text"
+          name="name"
+          value={name}
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          onChange={this.handleChange}
+          placeholder="Name"
+        />
+        <label className={css.label} htmlFor="number">
+          Phone number:
+        </label>
+        <input
+          className={css.input}
+          type="tel"
+          name="number"
+          value={number}
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+          onChange={this.handleChange}
+          placeholder="Phone number"
+        />
+        <button className={css.button} type="submit">
+          Add Contact
+        </button>
+      </form>
+    );
+  }
+}
+
+
 
 export default PhonebookForm;
